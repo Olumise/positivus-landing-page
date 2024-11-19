bd4d75253e22c018112a3f760426e47e1a289155
Olumise <theolumise@gmail.com>
2024-11-19 21:23:40 +0100

    Added more sections

---
+++ script.js
@@ -0,0 +1,6 @@
+const faqEl = document.querySelector(".faq-item"),
+    faqHead = document.querySelector(".faq-item-head"),
+      faqContent = document.querySelectorAll(".faq-info"),
+      faqIcon = document.querySelector(".faq-icon-close")
+
+      faqContent.style.display = "none";
